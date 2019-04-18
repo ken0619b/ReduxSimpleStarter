e8be36a10587f5f7e5b8e3e01a77a5398787d1b2
Kenichi Ukai <[ken151102@gmail.com]>
2019-04-18 09:43:33 +0900

    save until 50

--- testing/src/components/__test__/commentBox.test.js
+++ testing/src/components/__test__/commentBox.test.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapped;
 
 beforeEach(() => {
-  wrapped = mount(<CommentBox />);
+  wrapped = mount(
+  <Root>
+    <CommentBox />
+  </Root>
+  );
 })
 
 afterEach(() => {
   wrapped.unmount();
-})
+});
 
 it('has a text area and a buton', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
