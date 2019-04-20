973a1d757180b8854dc6af4dea98307b25567305
Kenichi Ukai <[ken151102@gmail.com]>
2019-04-20 17:02:19 +0900

    save until 89

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-import reduxPromise from 'redux-promise';
+import async from 'middlewares/async';
+import stateValidator from 'middlewares/stateValidator';
+
 export default ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(reduxPromise)
+    applyMiddleware(async, stateValidator)
   );
 
   return (
