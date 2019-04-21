026b9e74909cf23779ae9543f24f971157d74f4b
Kenichi Ukai <[ken151102@gmail.com]>
2019-04-21 19:19:30 +0900

    save until 106

---
+++ server/controllers/authentication.js
@@ -0,0 +1,44 @@
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  console.log('timestamp', timestamp)
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signup = function(req, res, next){
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+
+    if(err) { return next(err); }
+
+    // ion case of missing any params
+    if(!email || !password) {
+      return res.status(422).send({ error: 'Email or Password is missing'});
+    }
+
+    // If a user with email does exists, return an error
+    if(existingUser) {
+      return res.status(422).send({ error: 'Email is in use'});
+    }
+
+    // If a user with email does NOT exist, create and save record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if(err) { return next(err); }
+      console.log(tokenForUser(user))
+
+      // Respond to request indicatiing the user was creared
+      res.json({ token: tokenForUser(user) })
+    });
+  });
+};
