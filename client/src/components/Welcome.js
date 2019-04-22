78a5e98de376b5e66336031f1eb26f7ed9ff2a67
Kenichi Ukai <[ken151102@gmail.com]>
2019-04-22 21:07:13 +0900

    save until 125

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+  return <h3>Welcome! Sign up pr sign in!</h3>
+};
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
+import App from './components/app';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+
+ReactDOM.render(
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path='/' exact component={Welcome} />
+        <Route path='/signup' exact component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
+)
+
+
