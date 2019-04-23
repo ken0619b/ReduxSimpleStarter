68225bf7abf132594429d88b6f4194fd12ba9b88
Kenichi Ukai <[ken151102@gmail.com]>
2019-04-24 08:47:10 +0900

    finished!

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,13 +5,32 @@ export const signup = (formProps, callback) => async dispatch => {
   try {
   const response = await axios.post('http://localhost:3090/signup', formProps);
   dispatch({ type: AUTH_USER, payload: response.data.token });
+  localStorage.setItem('token', response.data.token);
   callback();
   } catch(e){
     dispatch({ type: AUTH_ERROR, payload: 'Email in use'});
   }
 };
 
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+  const response = await axios.post('http://localhost:3090/signin', formProps);
+  dispatch({ type: AUTH_USER, payload: response.data.token });
+  localStorage.setItem('token', response.data.token);
+  callback();
+  } catch(e){
+    dispatch({ type: AUTH_ERROR, payload: 'invalid login credentials'});
+  }
+};
 
+export const signout = () => {
+  localStorage.removeItem('token');
+
+  return {
+    type: AUTH_USER,
+    payload: ''
+  }
+};
 
 // これは
 // export const signup = ({ email, password }) => {
--- client/src/index.js
+++ client/src/index.js
@@ -10,10 +10,14 @@ import App from './components/app';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
 const store = createStore(
   reducers,
-  {},
+  {
+    auth: { authenticated: localStorage.getItem('token') }
+  },
   applyMiddleware(reduxThunk)
 );
 
@@ -24,6 +28,8 @@ ReactDOM.render(
         <Route path='/' exact component={Welcome} />
         <Route path='/signup' exact component={Signup} />
         <Route path='/feature' exact component={Feature} />
+        <Route path='/signout' exact component={Signout} />
+        <Route path='/signin' exact component={Signin} />
       </App>
     </BrowserRouter>
   </Provider>,
